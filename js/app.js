e4e2c385a004dab6bc2b0accfa73f9ec4496bac1
Ben Pinali <ben@benpinali.com>
2016-02-07 14:03:41 -0600

    Filled out conditions within function for comparing guess to secret number.

--- js/app.js
+++ js/app.js
@@ -33,6 +33,22 @@ $(document).ready(function () {
 
     function compareGuessToSecret(secret, number) {
         /* Triggered after successful validation */
+        var difference = Math.abs(secret - number);
+        if (difference >= 50) {
+            $('#feedback').text('Ice Cold!');
+        } else if (difference >= 30 && difference <= 49) {
+            $('#feedback').text('Cold!');
+
+        } else if (difference >= 20 && difference <= 29) {
+            $('#feedback').text('Warm!');
+
+        } else if (difference >= 10 && difference <= 19) {
+            $('#feedback').text('Hot!');
+        } else if (difference >= 1 && difference <= 9) {
+            $('#feedback').text('Very Hot!!');
+        } else {
+            $('#feedback').text('You got it. Well done!');
+        }
     }
 
     function countGuesses(guessCounter) {
